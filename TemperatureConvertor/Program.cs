a8f3b8a20bcc9fae48e0c6f5d11b215ee304dfa6
John Nassar <jnassar@metocean.com>
2017-11-20 11:51:44 -0400

    Add project files.

---
+++ TemperatureConvertor/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace com.metocean.convertor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
